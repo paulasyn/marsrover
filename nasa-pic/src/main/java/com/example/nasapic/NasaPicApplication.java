6e57a9dfb84c8b39794851906673e3b8073c7c04
Paul Asyn <paulasyn@gmail.com>
2019-10-07 21:59:06 -0500

    finished processing txt file, parsing dates.

--- nasa-pic/src/main/java/com/example/nasapic/NasaPicApplication.java
+++ nasa-pic/src/main/java/com/example/nasapic/NasaPicApplication.java
@@ -13,25 +13,29 @@ import org.springframework.web.client.RestTemplate;
 public class NasaPicApplication {
 
 	public static void main(String[] args) {
-		/*
-		* 1. Read from file
-		* 2. Process dates
-		* 3. Build api call
-		* 4. Call api
-		* 5. Display photos
-		* */
-
-
 		SpringApplication.run(NasaPicApplication.class, args);
+
+		// 1. Read from file
+		List<String> inputList = processFile();
+
+		// 2. Process dates
+		List<String> dateList = parseDates(inputList);
+
+		// 3. Build api call
+
+		// 4. Call api
+
+		// 5. Display photos
+	}
+
+	public static List<String> processFile(){
+		// read the file and save the entries to a list
 
 		String currentDirectory = System.getProperty("user.dir");
 		String filePath = currentDirectory + "/inputFile.txt";
-
-
 		List<String> inputList = new ArrayList<>();
 		File file = new File(filePath);
 
-		// read the file and save the entries to a list
 		try(Scanner scanner = new Scanner(file)){
 			int i = 0;
 			while(scanner.hasNextLine()){
@@ -40,6 +44,19 @@ public class NasaPicApplication {
 		} catch(FileNotFoundException e){
 			System.out.println(e);
 		}
+		return inputList;
 	}
+
+	public static List<String> parseDates(List<String> inputDates){
+		// process the input dates to match format
+
+        // TODO: figure out how to do last date format
+		List<String> formatDates = Arrays.asList("MM/dd/yy", "M-d-yyyy", "lastDate");
+		List<String> newDates = new ArrayList<>();
+		String wantedDate = "YYYY-MM-DD";
+
+		return newDates;
+	}
+
 
 }
