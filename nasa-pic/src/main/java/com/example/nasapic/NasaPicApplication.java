5a1cfffa9a3c0b5f840d4225f58ed678cbe23935
Paul Asyn <paulasyn@gmail.com>
2019-10-07 21:27:04 -0500

    read file and save to list

--- nasa-pic/src/main/java/com/example/nasapic/NasaPicApplication.java
+++ nasa-pic/src/main/java/com/example/nasapic/NasaPicApplication.java
@@ -1,5 +1,9 @@
 package com.example.nasapic;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+import java.lang.System.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +12,34 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class NasaPicApplication {
 
-	@Bean
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
-
 	public static void main(String[] args) {
+		/*
+		* 1. Read from file
+		* 2. Process dates
+		* 3. Build api call
+		* 4. Call api
+		* 5. Display photos
+		* */
+
+
 		SpringApplication.run(NasaPicApplication.class, args);
+
+		String currentDirectory = System.getProperty("user.dir");
+		String filePath = currentDirectory + "/inputFile.txt";
+
+
+		List<String> inputList = new ArrayList<>();
+		File file = new File(filePath);
+
+		// read the file and save the entries to a list
+		try(Scanner scanner = new Scanner(file)){
+			int i = 0;
+			while(scanner.hasNextLine()){
+				inputList.add(scanner.nextLine());
+			}
+		} catch(FileNotFoundException e){
+			System.out.println(e);
+		}
 	}
 
 }
