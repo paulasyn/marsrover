5689f226a3b8aea17f73aa4a01d08221183abf4c
Paul Asyn <paulasyn@gmail.com>
2019-10-16 16:48:29 -0500

    Added model for InputDate object after parsing

---
+++ nasa-pic/src/main/java/com/example/nasapic/model/InputDate.java
@@ -0,0 +1,20 @@
+package com.example.nasapic.model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class InputDate {
+
+    String date;
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDateString() {
+        return date;
+    }
+
+
+}
